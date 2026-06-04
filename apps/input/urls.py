5684e7e836beb51408171199022a8fc2ea18d703
parksoyun9084-cloud <parksoyun9084@gmail.com>
2026-06-04 22:48:25 +0900

    feat: 사용자 프로필 API 구현 #7

--- apps/input/urls.py
+++ apps/input/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import JDListCreateView, JDDetailView
+from .views import JDListCreateView, JDDetailView, UserProfileView
 
 urlpatterns = [
     path('jds', JDListCreateView.as_view(), name='jd-list-create'),
     path('jds/<uuid:jd_id>', JDDetailView.as_view(), name='jd-detail'),
+    path('users/me/profile', UserProfileView.as_view(), name='user-profile'),
 ]
